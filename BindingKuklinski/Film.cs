7ec3a476d838a789939e6af831202ac5e8291b3b
PatrykTheViking <patryk.bytow@wp.pl>
2023-05-20 22:58:15 +0200

    Dodaj pliki projektów.

---
+++ BindingKuklinski/Film.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingKuklinski
+{
+    public class Film : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private static Dictionary<string, ICollection<string>> powiązaneWłaściwości = new Dictionary<string, ICollection<string>>()
+        {
+            ["Tytuł"] = new string[] { "Tytuł" },
+            ["Reżyser"] = new string[] {"Reżyser"},
+            ["Wydawca"] = new string[] { "Wydawca" },
+            ["Nośnik"] = new string[] { "DVD", "VHS", "BlueRay" },
+            ["Data Wydania"] = new string[] { "Data Wydania" }
+        };
+
+        private void NotyfikujZmianę(
+            [CallerMemberName] string nazwaWłaściwości = null,
+            HashSet<string> jużZałatwione = null
+            )
+        {
+            if (jużZałatwione == null)
+                jużZałatwione = new HashSet<string>();
+            PropertyChanged?.Invoke(
+                this,
+                new PropertyChangedEventArgs(nazwaWłaściwości)
+                );
+            jużZałatwione.Add(nazwaWłaściwości);
+            if (powiązaneWłaściwości.ContainsKey(nazwaWłaściwości))
+                foreach (
+                    string powiązanaWłaściwość
+                    in
+                    powiązaneWłaściwości[nazwaWłaściwości]
+                    )
+                    if (jużZałatwione.Contains(powiązanaWłaściwość) == false)
+                        NotyfikujZmianę(powiązanaWłaściwość, jużZałatwione);
+        }
+
+        private string
+            tytuł,
+            reżyser,
+            wydawca;
+
+        private DateTime
+            dataWydania = DateTime.Now;
+
+        private enum
+            Nośnik
+        {
+            dvd,
+            vhs,
+            blueray
+        };
+
+/*        public Nośnik nośnik
+        { 
+            get => nośnik; 
+            set 
+            { 
+                nośnik = value; 
+                NotyfikujZmianę();
+            }
+        }*/
+
+public String Tytuł
+        {
+            get => tytuł;
+            set
+            {
+                tytuł = value;
+                NotyfikujZmianę();
+            }
+        }
+
+        public String Reżyser
+        {
+            get => reżyser;
+            set
+            {
+                reżyser = value;
+                NotyfikujZmianę();
+            }
+        }
+
+        public String Wydawca
+        {
+            get => wydawca;
+            set
+            {
+                wydawca = value;
+                NotyfikujZmianę();
+            }
+        }
+        public DateTime DataWydania
+        {
+            get => dataWydania;
+            set
+            {
+                dataWydania = value;
+                NotyfikujZmianę();
+            }
+        }
+
+
+
+        public string TytułReżyser => $"{tytuł}, Reżyser - {reżyser}";
+
+        public string SkrótSzczegółów => $"{TytułReżyser}, data wydania - {dataWydania}";
+    }
+}
---
+++ BindingKuklinski/ListaFilmów.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BindingKuklinski
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ListaFilmów.xaml
+    /// </summary>
+    public partial class ListaFilmów : Window
+    {
+
+        public ObservableCollection<Film> Filmy { get; } = new ObservableCollection<Film>();
+
+        ListBox lista;
+        public ListaFilmów()
+        {
+            DataContext = this;
+            InitializeComponent();
+            lista = (ListBox)FindName("Lista");
+        }
+
+        private void Edytuj(object sender, RoutedEventArgs e)
+        {
+            Film wybrany = (Film)lista.SelectedItem;
+            if (wybrany != null)
+                new WidokFilm(wybrany).Show();
+        }
+
+        private void Dodaj(object sender, RoutedEventArgs e)
+        {
+            Film nowy = new Film();
+            Filmy.Add(nowy);
+            new WidokFilm(nowy).Show();
+        }
+
+        private void Usuń(object sender, RoutedEventArgs e)
+        {
+            Film wybrany = (Film)lista.SelectedItem;
+            Filmy.Remove(wybrany);
+        }
+
+        private void Importuj(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Eksportuj(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        }
+    }
